95fa8cda067042dc08861dbf0d40fee21d47dbb1
LeDfee <yermakovdo@gmail.com>
2024-01-16 18:16:22 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Department salesDepartment = new Department(1, "Продажи");
+        Department itDepartment = new Department(2, "АйТи");
+
+        Employee emp1 = new Employee(666, "Артём Слезачек", "Менеджер по продажам", 42392348, salesDepartment);
+        Employee emp2 = new Employee(777, "Никита Зотов", "Инженер-программист", 5984594, itDepartment);
+
+        salesDepartment.addEmployee(emp1);
+        itDepartment.addEmployee(emp2);
+
+        salesDepartment.displayEmployees();
+        salesDepartment.displayTotalSalary();
+
+        itDepartment.displayEmployees();
+        itDepartment.displayTotalSalary();
+
+        emp1.promote(1000000000);
+    }
 }

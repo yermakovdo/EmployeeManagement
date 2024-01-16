b2e0c06eaf02751229b6c757cc1a59163b8619b5
LeDfee <yermakovdo@gmail.com>
2024-01-16 18:15:44 +0100

    Update Department.java

--- src/main/java/Department.java
+++ src/main/java/Department.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Department {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department {
+    private int departmentId;
+    private String name;
+    private List<Employee> employees;
+
+    public Department(int departmentId, String name) {
+        this.departmentId = departmentId;
+        this.name = name;
+        this.employees = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee employee) {
+        employees.add(employee);
+        employee.promote(1000);
+        System.out.println(employee.toString() + " был принят в " + name);
+    }
+
+    public void displayEmployees() {
+        System.out.println("Сотрудники в " + name + ":");
+        for (Employee employee : employees) {
+            System.out.println(employee.toString());
+        }
+    }
+
+    public void displayTotalSalary() {
+        double totalSalary = 0;
+        for (Employee employee : employees) {
+            totalSalary += employee.getSalary();
+        }
+        System.out.println("Вся зарплата в " + name + ": " + totalSalary);
+    }
 }
